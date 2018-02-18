88a942768ed926e93272af0a85c54ac8971ca287
zlira <olesia.hr@gmail.com>
2018-02-18 16:58:09 +0200

    Added button to add steps one at a time

--- main.js
+++ main.js
@@ -17,6 +17,18 @@ function getRandomPeaSeq(count) {
 }
 
 /**
+ * given the index of a step returns it's start and end
+ * in a sequence where each step is one pea longer than the previous
+ */
+function stepSlice(stepNum) {
+  const start = (stepNum - 1) * stepNum / 2;
+  return {
+    start: start,
+    end: start + stepNum,
+  };
+}
+
+/**
  * from sequence of peas to expanded list with state of
  * peaSeq at each step
  */
@@ -26,7 +38,7 @@ function expandPeaSeq(peaSeq) {
     stepState = peaSeq.slice(0, i).sort((d1, d2) => d1.color > d2.color ? 1 : -1)
     stepState = stepState.map((p, j) => ({
       ind: j, color: p.color, step: i,
-      xCoef: xCoef,
+      xCoef: xCoef
     }));
     expandedSeq = expandedSeq.concat(stepState);
     xCoef += 1 / (i * 2) + 1 / ((i + 1) * 2);
@@ -34,45 +46,66 @@ function expandPeaSeq(peaSeq) {
   return expandedSeq;
 }
 
-function drawRandomPeas(peaSeqSteps, shape='circle') {
-  const width = 1115,
-        height = 200;
-  let svg = d3.select('#pea-probability')
-              .append('svg')
-                .attr('height', height)
-                .attr('width', width);
-  //svg.append('rect')
-   //   .attr('width', '100%')
-   //   .attr('fill', '#017A57');
-   //   .attr('height', '100%')
-
-  if (shape === 'circle') {
-    svg.append('g').selectAll('circle')
-       .data(peaSeqSteps)
-       .enter().append('circle')
-       .attr('fill', d => d.color)
-       .attr('r', d => height / d.step/ 2)
-       .attr('cx', d => d.xCoef * height)
-       .attr('cy', d => (height / d.step / 2)  * 2 * (d.ind + .5));
-  } else if (shape === 'rect') {
-    svg.append('g').selectAll('rect')
-       .data(peaSeqSteps)
-       .enter().append('rect')
-       .attr('fill', d => d.color)
-       .attr('stroke', 'white')
-       .attr('stroke-width', .5)
-       .attr('width', d => height / d.step)
-       .attr('height', d => height / d.step)
-       .attr('x', d => d.xCoef * height - height / d.step / 2)
-       .attr('y', d => (height / d.step / 2)  * 2 * (d.ind + .5) - height / d.step / 2);
+class RandomPeas {
+  constructor (svgWidth, svgHeight, middleLineColor, backgroundColor,) {
+    this.svgWidth = svgWidth;
+    this.svgHeight = svgHeight;
+    this.backgroundColor = backgroundColor;
+    this.middleLineColor = middleLineColor;
+    this.svg = d3.select('#pea-probability')
+                 .append('svg')
+                   .attr('height', this.svgHeight)
+                   .attr('width', this.svgWidth);
+    this.addBackground();
+    this.peaGroup = this.svg.append('g').attr('id', 'pea-group');
   }
 
+  addBackground() {
+    if (this.backgroundColor) {
+      this.svg.append('rect')
+              .attr('width', '100%')
+              .attr('height', '100%')
+              .attr('fill', this.backgroundColor);
+    }
+  }
 
-  svg.append('line').attr('x1', 0).attr('y1', height / 2)
-                    .attr('x2', width).attr('y2', height / 2)
-                    .attr('stroke', 'grey').attr('stroke-width', '1px');
+  drawPeas(peaStepSeq) {
+    this.peaGroup
+        .selectAll('circle')
+        .data(peaStepSeq, (d, i) => i)
+        .enter().append('circle')
+          .attr('fill', d => d.color)
+          .attr('r', d => this.svgHeight / d.step/ 2)
+          .attr('cx', d => d.xCoef * this.svgHeight)
+          .attr('cy', d => (this.svgHeight / d.step / 2)  * 2 * (d.ind + .5));
+  }
+
+  drawMiddleLine() {
+    this.svg.append('line')
+            .attr('x1', 0).attr('y1', this.svgHeight / 2)
+            .attr('x2', this.svgWidth).attr('y2', this.svgHeight / 2)
+            .attr('stroke', this.middleLineColor).attr('stroke-width', '1px');
+  }
+
+  drawStepsTill(peaStepSeq, stepNum) {
+    const numPeasInculeded = stepSlice(stepNum).start;
+    this.drawPeas(peaStepSeq.slice(0, numPeasInculeded));
+  }
+
+  transitionStep() {
+    // move from prev step to the next with same peas
+    // clear a space for a new pea
+    // add a new pea
+  }
 }
 
+
 let peaSeqSteps = expandPeaSeq(getRandomPeaSeq(145));
-drawRandomPeas(peaSeqSteps);
-drawRandomPeas(peaSeqSteps, 'rect');
+let peasPic = new RandomPeas(1115, 200, 'white', '#017A57');
+let steps = 0;
+d3.select('#add-a-pea')
+  .on('click', function() {
+    steps += 1;
+    peasPic.drawStepsTill(peaSeqSteps, steps);
+  });
+peasPic.drawMiddleLine();
