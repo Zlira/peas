f6abbfaa6afc9d41ac958d115f24f30df81884d5
zlira <olesia.hr@gmail.com>
2018-02-21 00:12:25 +0200

    This is very horrible but somewhat working random peas

--- main.js
+++ main.js
@@ -34,14 +34,25 @@ function stepSlice(stepNum) {
  */
 function expandPeaSeq(peaSeq) {
   let expandedSeq = [], stepState, xCoef=0.5;
-  for (let i=1; i <= peaSeq.length; i++) {
-    stepState = peaSeq.slice(0, i).sort((d1, d2) => d1.color > d2.color ? 1 : -1)
-    stepState = stepState.map((p, j) => ({
-      ind: j, color: p.color, step: i,
-      xCoef: xCoef
+  let sortedStepState = [];
+  for (let step=1; step <= peaSeq.length; step++) {
+    sortedStepState = [];
+    stepState = peaSeq.slice(0, step).reverse();
+    for (let pea of stepState) {
+        // todo this hardcoded color is no good
+        if (pea.color !== '#A9CF46') {
+          sortedStepState.push(pea);
+        } else {
+          sortedStepState.unshift(pea);
+        }
+    };
+    stepState = sortedStepState.map((p, j) => ({
+      ind: j, color: p.color, step: step,
+      xCoef: xCoef, isNew: p.ind === step - 1,
+      id: step + '.' + p.ind,
     }));
     expandedSeq = expandedSeq.concat(stepState);
-    xCoef += 1 / (i * 2) + 1 / ((i + 1) * 2);
+    xCoef += 1 / (step * 2) + 1 / ((step + 1) * 2);
   };
   return expandedSeq;
 }
@@ -70,14 +81,16 @@ class RandomPeas {
   }
 
   drawPeas(peaStepSeq) {
-    this.peaGroup
-        .selectAll('circle')
-        .data(peaStepSeq, (d, i) => i)
-        .enter().append('circle')
-          .attr('fill', d => d.color)
-          .attr('r', d => this.svgHeight / d.step/ 2)
-          .attr('cx', d => d.xCoef * this.svgHeight)
-          .attr('cy', d => (this.svgHeight / d.step / 2)  * 2 * (d.ind + .5));
+    const circles = this.peaGroup
+                        .selectAll('circle')
+                        .data(peaStepSeq); //d => d.totalInd)
+    circles.enter().append('circle')
+           .merge(circles)
+           .attr('fill', d => d.color)
+           .attr('r', d => this.svgHeight / d.step / 2)
+           .attr('cx', d => d.xCoef * this.svgHeight)
+           .classed('new', d => d.isNew)
+           .attr('cy', d => (this.svgHeight / d.step / 2)  * 2 * (d.ind + .5));
   }
 
   drawMiddleLine() {
@@ -88,8 +101,46 @@ class RandomPeas {
   }
 
   drawStepsTill(peaStepSeq, stepNum) {
-    const numPeasInculeded = stepSlice(stepNum).start;
-    this.drawPeas(peaStepSeq.slice(0, numPeasInculeded));
+    const self = this;
+    const stepBounds = stepSlice(stepNum);
+    const currStep = peaStepSeq.slice(stepBounds.start, stepBounds.end);
+    // todo is this stupid? think about it
+    const shiftedCurrStep = currStep
+    .filter(d => !d.isNew)
+    .map(d => ({
+      ind: d.ind,
+      color: d.color,
+      step: d.step - 1,
+      isNew: d.isNew,
+      // todo make a function for this shit beacause i don't understand it
+      xCoef: d.xCoef - 1 / ((stepNum - 1) * 2) - 1 / (stepNum * 2)
+    }));
+    const toDraw = peaStepSeq.slice(0, stepBounds.start).concat(shiftedCurrStep);
+    //console.log(stepNum);
+    //console.log(shiftedCurrStep);
+    this.drawPeas(toDraw);
+    let updateGroup = this.peaGroup.selectAll('circle')
+                          .data(peaStepSeq.slice(0, stepBounds.end)); //d => d.totalInd)
+    // console.log(updateGroup);
+    updateGroup.classed('new', d => d.isNew)
+               .transition().duration(2000)
+               .attr('fill', d => d.color)
+               .attr('cx', d => d.xCoef * this.svgHeight)
+               .attr('r', d => this.svgHeight / d.step / 2)
+               .attr('cy', d => (this.svgHeight / d.step / 2)  * 2 * (d.ind + .5));
+    // todo chain transitions
+    updateGroup.enter().append('circle')
+          .attr('id', d => d.totalInd)
+          .attr('fill', d => d.color)
+          .classed('new', d => d.isNew)
+          .attr('r', 0)
+          .attr('cx', d => d.xCoef * this.svgHeight)
+          .attr('cy', d => (this.svgHeight / d.step / 2)  * 2 * (d.ind + .5))
+          .transition().duration(2000).delay(2000)
+          .each(function() {
+            console.log(d3.select(this));
+            d3.select(this).attr('r', d => self.svgHeight / d.step / 2)
+          });
   }
 
   transitionStep() {
